024af5b50d0241389509255b2b018b54d6ce3044
Redmen74 <vrlpz62@gmail.com>
2024-08-06 12:43:58 -0500

    Segundo avance

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
-const router = require('./router/router')
+const express = require('express');
+const app = express();
+const conectar = require("./database/db");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false}));
+
+app.use(express(JSON));
 const port = 3000
 
 app.set('view engine','ejs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('login')
+})
+
+app.listen(port, () => {
+    console.log(`Servidor ejecutandose http://localhost:${port}`)
 })
