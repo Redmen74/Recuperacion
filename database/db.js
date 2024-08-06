780eadf15d21d414428dac7ee7372bddfd41bc22
Redmen74 <vrlpz62@gmail.com>
2024-08-06 12:27:04 -0500

    Primer avance

---
+++ database/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const conectar = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"login",
+});
+
+conectar.connect((error) => {
+    if(error)
+    {
+        console.log('error de conexion')
+    }
+    else{
+        console.log('conectado a la base de datos')
+    }
+});
+
+module.exports = conectar;
